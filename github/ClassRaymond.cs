09403bb6305852106ce606333517efdc4e092485
rlaberge <1495778@college-em.qc.ca>
2015-04-02 21:08:19 -0400

    Nouvelle classe de Raymond

---
+++ github/ClassRaymond.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace github
+{
+    class ClassRaymond
+    {
+        //nouvelle classe de Raymond
+    }
+}
